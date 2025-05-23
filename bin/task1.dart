6e621e0aa6220cca978e21cb17f2df77f7bc397c
unknown <aanuseyi091@gmail.com>
2025-05-23 12:25:57 +0100

    initialcommit

--- bin/task1.dart
+++ bin/task1.dart
@@ -3,53 +3,38 @@ import 'dart:io';
 import 'package:dart_application/task1.dart';
 
 void main() {
-  print("Welcome to mortage bank");
+  print("Welcome to Aanuoluwapo bank");
 
-  // int check_balance = 1;
-  // int deposit = 2;
-  // int withdraw = 3;
+  while (true) {
+    print("Please select any of the following options:");
+    print("1 - Check Balance");
+    print("2 - Deposit");
+    print("3 - Withdraw");
+    print("4 - Exit");
 
-  print("Please select any of the following of the following \n 1 - Check-Balance \n 2 - Deposit \n 3 - withdraw ");
+    int option = int.parse(stdin.readLineSync()!);
 
-  int option = int.parse(stdin.readLineSync()!)
-  
-  ;
-
-
-
-  if (option == 1) {
-
-    print("Your account balance is :");
-    print(account_balance());
-  } 
-  
-  else if (option == 2) {
-    print("How much do you intend to deposit :");
-
-    int amount = int.parse(stdin.readLineSync()!);
-
-    print("$amount succesfully deposited new balance is:");
-
-      // print(deposit(amount: amount, balance: account_balance()));
-
+    if (option == 1) {
+      print("Your account balance is:");
+      print(account_balance());
+    } else if (option == 2) {
+      print("How much do you want to deposit:");
+      int amount = int.parse(stdin.readLineSync()!);
       int result = mine(
-    amount: amount,
-    balance: account_balance(),
-  );
-
-  print( 'New balance: $result'); 
-  
-
-
-  } else if (option == 3) {
-      print("How much do you intend to withdraw :");
-
-    int withdraw_amount = int.parse(stdin.readLineSync()!);
-
-    int withdrawed = withdraw(amount: withdraw_amount, balance: account_balance());
-
-    print('u have succefully withdrawed ; $withdrawed');
+        amount: amount,
+        balance: account_balance(),
+      );
+      print('New balance: $result');
+    } else if (option == 3) {
+      print("How much do you want to withdraw:");
+      int withdrawAmount = int.parse(stdin.readLineSync()!);
+      int withdrawed = withdraw(amount: withdrawAmount, balance: account_balance());
+      print('Your withdrawal is succcesful, your new balance is $withdrawed');
+    } else if (option == 4) {
+      print("Thank you for banking with us. Goodbye!");
+      break;
+    } else {
+      print("Invalid option. Please try again.");
+    }
   }
-
-
 }
